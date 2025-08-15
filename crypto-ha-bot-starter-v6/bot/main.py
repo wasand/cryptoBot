27e37c342bb5ee8eb764bd7023f384e3d62e6078
wasand <wasand@o2.pl>
2025-08-15 18:24:22 +0200

    first commit

---
+++ crypto-ha-bot-starter-v6/bot/main.py
@@ -0,0 +1,2 @@
+print("Crypto HA Bot v6 - starter uruchomiony (przykładowa wersja)")
+# Tutaj byłaby logika PnL, EMA, flag, kolektorów danych itd.
---
+++ crypto-ha-bot-starter-v6/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Instalacja Crypto HA Bot Starter v6"
+docker compose up -d --build
